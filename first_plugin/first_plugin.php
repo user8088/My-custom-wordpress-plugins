28329b6c1904f85116deca573b94538f85d5e65b
Asim <asimmahmood11110@gmail.com>
2024-06-27 16:29:45 +0500

    initial commit

---
+++ first_plugin/first_plugin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package FirstPlugin
+ */
+
+ /*
+    Plugin Name: FirstPlugin
+    Plugin URI:
+    Description: This is my very first custom wordpress plugin. My goal is understand how wp wordpress development works so I can create my own awesome plugins!
+    Version: 1.0.0
+    Author: Asim Mahmood
+    Author URI:
+    License: GPLv2 or later
+    Text Domain: asims-plugin
+ */
+
+ /*
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
